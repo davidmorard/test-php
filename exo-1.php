1b16f64f82cab27642b34841f4c70268621b838f
davidmorard <davidmorard@gmail.com>
2019-05-05 19:49:03 +0200

    firt exo on while and foreach

--- exo-1.php
+++ exo-1.php
@@ -5,19 +5,14 @@ Chaque note est sauvegardée dans un tableau $notes (pensez $notes[])
 à la fin on affiche le tableau de note sous forme de liste
 */  
 
-
-    
-
-   
-
 $notes = [];
-$action = null;
-
 // TANT QUE l'utilisateur ne tape pas "fin"
-while ($action !== 'fin') {
+while (true) {
     $action = (int)readline('Entrez une nouvelle note (ou \'fin\' pour terminer la saisie) : ');
     // On ajoute la note tapée au tableau notes
-    if ($action !== 'fin') {
+    if ($action === 'fin') {
+        break;
+    } else {
         $notes[] = (int)$action;
     }
 }
@@ -27,3 +22,4 @@ foreach ($notes as $note) {
     // ON AFFICHE "- note"
     echo "- $note \n";
 }
+?>
