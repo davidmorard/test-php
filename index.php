47314d34465d265ec62b41597d2fbe8991501793
David Morard <44342510+davidmorard@users.noreply.github.com>
2024-02-04 23:57:09 +0100

    first integration php code

--- index.php
+++ index.php
@@ -1,3 +1,39 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Ceci est une page de test avec des balises PHP
 
-echo "Hello world !";
+        </title>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+        <h2>Page de test
+
+        </h2>
+        <p>
+            Cette contient du code HTML avec des balises PHP.
+            <br />
+            <?php echo("C'est ici qu'il y a du code PHP avec l'instruction 'echo'")
+            ?>
+            Voici quelques petits tests :
+        </p>
+
+        <ul>
+            <li style="color: blue;">Texte en bleu
+        </li>
+            <li style="color: red;">Texte en rouge
+        </li>
+            <li style="color green;">Texte en vert
+        </li>
+        </ul>
+
+        <?php
+        /* Encore du PHP
+        Toujours du PHP */
+        ?>
+    </body>
+</html>
+
+
+
+
